669043f18b355cfbc93044af7c82c7a4ef580dcf
robinruden <145325673+robinruden@users.noreply.github.com>
2024-01-25 14:29:44 +0100

    robin gren

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-function news(){
+function newsTest(){
     const url = `https://api.worldnewsapi.com/search-news?text=saab/source-countries=sv/&language=en&api-key=d7d656cae7b2484682b91942b7092e48`;
 
     axios.get(url)
@@ -14,5 +14,33 @@ function news(){
             console.log("DET BLEV FEEEELL!!!", error))
 };
 
-news();
+newsTest();
 
+function finHund(){
+    const url = 'https://dog.ceo/api/breeds/image/random';
+    axios.get(url)
+        .then(response => {
+            const data = response.data;
+            const hundGrej = document.getElementById('hund');
+            hundGrej.src = data.message;
+
+
+        })
+    };
+
+    finHund();
+
+
+
+
+function trump(){
+    const url = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
+    axios.get(url)
+        .then(response => {
+            const data = response.data;
+            const trumpGrej = document.getElementById('trump');
+            trumpGrej.textContent = data.message;
+        })
+}
+
+trump();
